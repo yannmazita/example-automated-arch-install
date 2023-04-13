6f703ad05e1a87fc5566793efbd634d4eb5943eb
Yann Mazita <dev.mazita@gmail.com>
2023-04-14 00:26:53 +0200

    Sudo fix

--- scripts/post-install_script.sh
+++ scripts/post-install_script.sh
@@ -104,13 +104,13 @@ function configurationsPropres()
             ;;
         "serveur-bdd")
             cd /tmp || exit # parce que postgre envoie un message d'erreur inutile
-            sudo su postgres -c "initdb -D /var/lib/postgres/data --data-checksums"
+            sudo su -l postgres -c "initdb -D /var/lib/postgres/data --data-checksums"
             sudo curl "https://raw.githubusercontent.com/yannmazita/example-automated-arch-install/main/config/serveur-bdd/var/lib/postgres/data/pg_hba.conf" -o /var/lib/postgres/data/pg_hba.conf
             sudo curl "https://raw.githubusercontent.com/yannmazita/example-automated-arch-install/main/config/serveur-bdd/var/lib/postgres/data/postgresql.conf" -o /var/lib/postgres/data/postgresql.conf
             sudo systemctl start postgresql.service
             sudo systemctl enable postgresql.service
-            sudo su postgres -c "createuser admin --superuser"
-            sudo su postgres -c "createdb baseDeDonnees -O admin"
+            sudo su -l postgres -c "createuser admin --superuser"
+            sudo su -l postgres -c "createdb baseDeDonnees -O admin"
 
             sudo curl "https://raw.githubusercontent.com/yannmazita/example-automated-arch-install/main/config/serveur-bdd/etc/ntp.conf" -o /etc/ntp.conf
             sudo systemctl start ntpd.service
