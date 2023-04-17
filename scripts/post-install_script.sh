e70bd86c9d2fdd5139d16ab91b3d83f29c1d40f4
Yann Mazita <dev.mazita@gmail.com>
2023-04-17 14:23:35 +0200

    Updated Django environment variables

--- scripts/post-install_script.sh
+++ scripts/post-install_script.sh
@@ -84,11 +84,9 @@ function deployerServeurWeb()
     git clone https://github.com/yannmazita/example-server.git
     cd example-server || exit
     echo "SECRET_KEY = '$(openssl rand -hex 40)'" > src/my_website/.env
-    {
-        echo "DJANGO_SUPERUSER_USERNAME = admin";
-        echo "DJANGO_SUPERUSER_PASSWORD = master";
-        echo "DJANGO_SUPERUSER_EMAIL = admin@admin.admin";
-    } >> src/my_website/.env 
+    echo "export DJANGO_SUPERUSER_USERNAME = 'admin'" | sudo tee -a /etc/zshenv 1&> /dev/null
+    echo "export DJANGO_SUPERUSER_PASSWORD = 'master'" | sudo tee -a /etc/zshenv 1&> /dev/null
+    echo "export DJANGO_SUPERUSER_EMAIL = 'admin@admin.admin'" | sudo tee -a /etc/zshenv 1&> /dev/null
     poetry install
     poetry run python src/my_website/manage.py migrate
     poetry run python src/my_website/manage.py createsuperuser --noinput --username "$DJANGO_SUPERUSER_USERNAME" --email "$DJANGO_SUPERUSER_EMAIL" --password "$DJANGO_SUPERUSER_PASSWORD"
